86c4c8a73d175b20b55919fccc3889e392535052
Vishal Gupta <vishal.3.gupta@atos.net>
2023-07-10 01:37:06 +0530

    Modified some files

--- tests.py
+++ tests.py
@@ -4,9 +4,9 @@ from main import to_upper
 
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
-        name = "Shubham"
+        name = "VishalDevOps"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "SHUBHAM")
+        self.assertEqual(upper_name, "VISHALDEVOPS")
 
 
 if __name__ == '__main__':
