4b5e503b514acbb017efda4b72b5efa632bde5ed
Vishal Gupta <vishal.3.gupta@atos.net>
2023-07-09 23:21:38 +0530

    Name changed in main file

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def say_hello(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    name = 'TrainWithShubham'
+    name = 'Vishal-DevOps'
     say_hello(name)
     up = to_upper(name)
     print(up)
