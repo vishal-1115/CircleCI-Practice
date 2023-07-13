5f8908a4ba61b10243400b42a6752361e0ce3935
Vishal Gupta <vishal.3.gupta@atos.net>
2023-07-13 22:19:13 +0530

    Added some changes

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ def say_hello(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     name = 'Prashant-DevOps'
-    say(name)
+    say_hello(name)
     up = to_upper(name)
     print(up)
